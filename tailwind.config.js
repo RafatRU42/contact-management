30210b38e570514924846f542daf7a6b13ccdab6
RafatRU42 <rmrafat420@gmail.com>
2023-08-24 17:42:30 +0600

    tailwind css installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  daisyui: {
+    themes: ["light"],
+  },
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,5 +10,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
